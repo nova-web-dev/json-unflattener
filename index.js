c2680be19f345d88f7b180e0ec90a32fdbbac5be
Carl Eiserman <carleiserman@gmail.com>
2017-12-06 09:01:59 +0200

    feat: added fake data

---
+++ index.js
@@ -0,0 +1,47 @@
+const _ = require('lodash');
+
+const flatData = [{
+        Id: 2,
+        Name: "Group1",
+        GroupType: 2,
+        ParentId: 4,
+        GroupKey: "group1",
+    },
+    {
+        Id: 4,
+        Name: "Group3",
+        GroupType: 2,
+        ParentId: 5,
+        GroupKey: "group1"
+    },
+    {
+        Id: 3,
+        Name: "Group2",
+        GroupType: 2,
+        ParentId: 1,
+        GroupKey: "group2"
+    },
+    {
+        Id: 5,
+        Name: "Collection 1",
+        GroupType: 1,
+        ParentId: 3,
+        GroupKey: null
+    },
+    {
+        Id: 6,
+        Name: "Collection 1",
+        GroupType: 1,
+        ParentId: 1,
+        GroupKey: null
+    },
+    {
+        Id: 7,
+        Name: "Collection 1",
+        GroupType: 1,
+        ParentId: 5,
+        GroupKey: null
+    }
+];
+
+const sortedItems = _.sortBy(flatData, "Id");
