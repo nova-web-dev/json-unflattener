d011aa99f9681526c18f8a5d7d07741ef38abe71
Carl Eiserman <carleiserman@gmail.com>
2017-12-06 09:31:44 +0200

    feat: added solution (recursive)

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 const _ = require('lodash');
+const fs = require('fs');
 
 const {data} = require('./data.js');
 const sortedItems = _.sortBy(data, "Id");
 
-console.log(sortedItems);
+unflatten(sortedItems);
+
+function unflatten(input) {
+    const finalItems = [];
+    const rootItems = input.filter(item => item.ParentId === 1);
+    
+    for (const item of rootItems) {
+        finalItems.push(item);
+    }
+
+    for (const item of finalItems) {
+        findAllChildren(item, input);
+    }
+
+    fs.writeFile("results.json", JSON.stringify(finalItems, null, 4), (error) => {
+        console.log("wrote file!")
+    });
+}
+
+function findAllChildren(item, flatArray) {
+    item['children'] = []; // initialize
+    const itemId = item.Id;
+    const children = flatArray.filter(child => child.ParentId === itemId);
+    item['children'] = children;
+
+    for (const child of item['children']) {
+        findAllChildren(child, flatArray);
+    }
+}
